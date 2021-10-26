620606569bc5fe0d6e3efdb48f53a09bfc4f9e6c
Codexgrey <thecodexgrey@gmail.com>
2021-10-26 11:35:09 +0100

    fixing gitignore

--- attendance/views.py
+++ attendance/views.py
@@ -1,3 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
 
 # Create your views here.
+
+def test_view(request):
+    return HttpResponse('Super nice view!')
