9a533ddc0f445869d5c4735e29cfe0be74751f28
Codexgrey <thecodexgrey@gmail.com>
2021-10-28 15:38:37 +0100

    testing foreign key relationships

--- attendance/models.py
+++ attendance/models.py
@@ -1,9 +1,25 @@
 from django.db import models
 
 # Create your models here.
+class Student(models.Model):
+    name = models.CharField(max_length=32)
+    gender = models.CharField(max_length=15)
+    dob = models.CharField(max_length=24)
+    age = models.IntegerField(default=18)
+    track = models.CharField(max_length=50)
+    mentor = models.CharField(max_length=50)
+    goals = models.TextField()
+    # date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+
+
 class Book(models.Model):
     title = models.CharField(max_length=300)
     no_of_pages = models.IntegerField(default=10)
+    # student = models.OneToOneField(Student, on_delete=models.CASCADE)
+    student = models.ForeignKey(Student, on_delete=models.CASCADE, related_name="books")
     author = models.CharField(max_length=300)
     body = models.TextField()
     isbn = models.CharField(max_length=15, null=True, blank=True)
@@ -14,14 +30,3 @@ class Book(models.Model):
         return self.title
 
 
-class Student(models.Model):
-    name = models.CharField(max_length=32)
-    gender = models.CharField(max_length=15)
-    dob = models.CharField(max_length=24)
-    age = models.IntegerField(default=18)
-    track = models.CharField(max_length=50)
-    mentor = models.CharField(max_length=50)
-    goals = models.TextField()
-
-    def __str__(self):
-        return self.name
