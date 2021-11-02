0c7ccabdd878582ee3a47f801c671eff8a638427
Codexgrey <thecodexgrey@gmail.com>
2021-11-02 12:17:59 +0100

    account app

--- attendance/views.py
+++ attendance/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render, HttpResponse
+from .models import Book
+
 
 # Create your views here.
 def test_view(request):
-    return render(request, 'test.html')
+    myBooks = Book.objects.all()
+    context = { 'books': myBooks }
+
+    return render(request, 'test.html', context)
 
 def home_page(request):
     return HttpResponse("I'm supposed to be the Home Page. 😒")
